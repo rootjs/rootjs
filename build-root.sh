32c0bab9d13884eaa64e4b5798d28c0ac3e1b9d5
Jonas Schwabe <jonas@jnugh.de>
2016-01-13 13:27:56 +0100

    Try using travis cache to speed up instaling of ROOT

--- build-root.sh
+++ build-root.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+CC="gcc-4.8"
+
 #Install ROOT devDependencies
 sudo apt-get update -qq
 sudo apt-get install -qq git dpkg-dev make g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev
 
 #Clone ROOT
-cd ..
+cd $HOME
 if [ ! -d root ]; then
   git clone https://github.com/root-mirror/root.git root
 fi
@@ -13,12 +15,10 @@ cd root
 git pull
 
 #Build ROOT
-cd ..
+cd $HOME
 if [ -d rootbuild ]; then
   cd rootbuild
-fi
-
-if [ ! -d rootbuild ]; then
+else
   mkdir rootbuild
   cd rootbuild
   cmake ../root
