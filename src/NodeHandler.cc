72790bdae05ee75ab200d1d3391ae7266e9b519c
Jonas Schwabe <jonas@jnugh.de>
2016-01-14 22:24:41 +0100

    Add Doxygen support

--- src/NodeHandler.cc
+++ src/NodeHandler.cc
@@ -2,6 +2,23 @@
 
 namespace RootJS {
   void NodeHandler::initialize(v8::Local<v8::Object> exports, v8::Local<v8::Object> module) {
+    if(!initialized) {
+      instance = new NodeHandler(exports);
+      instance->exposeROOT();
+    } else {
+      v8::ThrowException(v8::String::New("The NodeHandler can only be initialized once."));
+    }
+  }
+
+  NodeHandler::NodeHandler(v8::Local<v8::Object> exports) {
+    this->exports = exports;
+  }
+
+  void NodeHandler::exposeROOT() {
+    exposeGlobals();
+  }
+
+  void NodeHandler::exposeGlobals() {
     
   }
 }
--- src/NodeHandler.h
+++ src/NodeHandler.h
@@ -3,13 +3,30 @@
 
 #include <v8.h>
 #include <node.h>
+#include <TClassRef.h>
 
 namespace RootJS {
 
   class NodeHandler {
+  private:
+    bool initialized;
+    v8::Persistent<v8::Object> rootJS;
+    NodeHandler instance;
+    v8::Local<v8::Object> exports;
 
-    public:
-      static void initialize(v8::Local<v8::Object>, v8::Local<v8::Object>);
+    /* Private constructor - Singleton */
+    NodeHandler(void);
+
+    void exposeROOT();
+    void exposeGlobalFunctions();
+    void exposeGlobals();
+    void exposeMacros();
+    void exposeClasses();
+    void exposeClass(TClassRef klass);
+
+  public:
+    static void initialize(v8::Local<v8::Object>, v8::Local<v8::Object>);
+    v8::Local<v8::Object> getExports(void);
   };
 
   NODE_MODULE(rootjs, NodeHandler::initialize);
