0ec983551ce50c6c0a749c5d32962312e3435d73
chhas <utdtq@student.kit.edu>
2016-01-15 16:56:14 +0100

    add ObjectProxy class

---
+++ src/ObjectProxy.cpp
@@ -0,0 +1,13 @@
+
+#include "ObjectProxy.h"
+
+namespace RootJS {
+
+ObjectProxy::ObjectProxy() {
+
+}
+
+ObjectProxy::~ObjectProxy() {
+}
+
+}
---
+++ src/ObjectProxy.h
@@ -0,0 +1,15 @@
+
+#ifndef SRC_OBJECTPROXY_H_
+#define SRC_OBJECTPROXY_H_
+
+namespace RootJS {
+
+class ObjectProxy {
+public:
+	ObjectProxy();
+	virtual ~ObjectProxy();
+};
+
+}
+
+#endif /* SRC_OBJECTPROXY_H_ */
