fc3a16f3bdbc8067de28ccdffbd20e761dd7fb3d
theo <tbeffart@gmail.com>
2016-01-24 14:18:49 +0100

    modify ObjectProxy constructor

--- src/ObjectProxy.cc
+++ src/ObjectProxy.cc
@@ -3,59 +3,49 @@
 #include <TObject.h>
 #include <TGlobal.h>
 
-RootJS::ObjectProxy::ObjectProxy(const TDataMember& type, TClassRef scope) :
-	Proxy(nullptr, type, scope) {
+RootJS::ObjectProxy::ObjectProxy(TDataMember &type, TClassRef scope)
+    : Proxy(nullptr, type, scope) {}
+RootJS::ObjectProxy::ObjectProxy(TGlobal &type, TClassRef scope)
+    : Proxy(nullptr, type, scope) {}
 
+RootJS::ObjectProxy::~ObjectProxy() {}
+
+const TDataMember &RootJS::ObjectProxy::getType() {
+  return dynamic_cast<const TDataMember &>(type);
 }
 
-RootJS::ObjectProxy::ObjectProxy(void *object, const TGlobal & type,
-                                 TClassRef scope) :
-	Proxy(object, type, scope) {
-
-}
-
-RootJS::ObjectProxy::~ObjectProxy() {
-
-}
-
-const TDataMember& RootJS::ObjectProxy::getType() {
-	return dynamic_cast<const TDataMember&>(type);
-}
-
-void RootJS::ObjectProxy::set(ObjectProxy& value) {
-	// TODO: validate type equality
-	address = value.getAddress();
+void RootJS::ObjectProxy::set(ObjectProxy &value) {
+  // TODO: validate type equality
+  address = value.getAddress();
 }
 
 v8::Local<v8::Value> RootJS::ObjectProxy::get() {
-	// objects just return their holder - i.e the proxy member
-	return getProxy();
+  // objects just return their holder - i.e the proxy member
+  return getProxy();
 }
 
 void RootJS::ObjectProxy::setProxy(v8::Local<v8::Object> proxy) {
-	this->proxy.Reset(v8::Isolate::GetCurrent(), proxy);
+  this->proxy.Reset(v8::Isolate::GetCurrent(), proxy);
 }
 
 v8::Local<v8::Object> RootJS::ObjectProxy::getProxy() {
-	return v8::Local<v8::Object>::New(v8::Isolate::GetCurrent(), proxy);
+  return v8::Local<v8::Object>::New(v8::Isolate::GetCurrent(), proxy);
 }
 
-bool RootJS::ObjectProxy::isPrimitive() {
-	return false;
-}
+bool RootJS::ObjectProxy::isPrimitive() { return false; }
 
 bool RootJS::ObjectProxy::isTemplate() {
-	return false; //TODO
+  return false; // TODO
 }
 
 bool RootJS::ObjectProxy::isGlobal() {
-	return false; //TODO
+  return false; // TODO
 }
 
 bool RootJS::ObjectProxy::isConst() {
-	return false; //TODO
+  return false; // TODO
 }
 
 bool RootJS::ObjectProxy::isStatic() {
-	return false; //TODO
+  return false; // TODO
 }
--- src/ObjectProxy.h
+++ src/ObjectProxy.h
@@ -11,96 +11,90 @@
 
 namespace RootJS {
 
-	/**
-	 *The ObjectProxy class is used to represent ROOT objects.
-	 *It differentiates between primitive and non-primitive object types.
-	 */
-	class ObjectProxy: public Proxy {
+/**
+ *The ObjectProxy class is used to represent ROOT objects.
+ *It differentiates between primitive and non-primitive object types.
+ */
+class ObjectProxy : public Proxy {
 
-	public:
+public:
+  /**
+   * Create a new ObjectProxy of a TObject.
+   *
+   * @param type
+   * 			the type of the encapsulated object
+   *
+   * @param scope
+   *			the scope of the encapsulated object
+   */
+  ObjectProxy(TDataMember &type, TClassRef scope);
 
-		/**
-		 * Create a new ObjectProxy.
-		 *
-		 * @param type
-		 * 			the type of the encapsulated object
-		 *
-		 * @param scope
-		 *			the scope of the encapsulated object
-		 */
-		ObjectProxy(const TDataMember& type, TClassRef scope);
+  /**
+   * Create a new ObjectProxy of a TGlobal.
+   *
+   * @param type
+   * 			the type of the encapsulated object
+   *
+   * @param scope
+   *			the scope of the encapsulated object
+   */
+  ObjectProxy(TGlobal &type, TClassRef scope);
 
-		/**
-		 * Creates a new ObjectProxy.
-		 *
-		 * @param object
-		 *			the memory address of the encapsulated object
-		 *
-		 * @param type
-		 * 			the type of the encapsulated object
-		 *
-		 * @param scope
-		 *			the scope of the encapsulated object
-		 */
-		ObjectProxy(void *object, const TGlobal& type, TClassRef scope);
+  virtual ~ObjectProxy();
 
-		virtual ~ObjectProxy();
+  /**
+   * Return the meta information about the type of the encapsulated object.
+   *
+   * @return the meta information about the type of the encapsulated object.
+   */
+  const TDataMember &getType();
 
-		/**
-		 * Return the meta information about the type of the encapsulated object.
-		 *
-		 * @return the meta information about the type of the encapsulated object.
-		 */
-		const TDataMember& getType();
+  /**
+   * Assign the specified value to this ObjectProxy.
+   *
+   * @param value
+   * 			the value to assign to this ObjectProxy
+   */
+  virtual void set(ObjectProxy &value);
 
-		/**
-		 * Assign the specified value to this ObjectProxy.
-		 *
-		 * @param value
-		 * 			the value to assign to this ObjectProxy
-		 */
-		virtual void set(ObjectProxy& value);
+  /**
+   * Return the encapsulating javascript value.
+   *
+   * @return the encapsulating javascript value
+   */
+  virtual v8::Local<v8::Value> get();
 
-		/**
-		 * Return the encapsulating javascript value.
-		 *
-		 * @return the encapsulating javascript value
-		 */
-		virtual v8::Local<v8::Value> get();
+  /**
+   * Set the encapsulating javascript object.
+   *
+   * @param proxy
+   * 			the encapsulating javascript object
+   *
+   */
+  virtual void setProxy(v8::Local<v8::Object> proxy);
 
-		/**
-		 * Set the encapsulating javascript object.
-		 *
-		 * @param proxy
-		 * 			the encapsulating javascript object
-		 *
-		 */
-		virtual void setProxy(v8::Local<v8::Object> proxy);
+  /**
+   * Return the encapsulating javascript object.
+   *
+   * @return the encapsulating javascript object
+   */
+  virtual v8::Local<v8::Object> getProxy();
 
-		/**
-		 * Return the encapsulating javascript object.
-		 *
-		 * @return the encapsulating javascript object
-		 */
-		virtual v8::Local<v8::Object> getProxy();
+  /**
+   * Check if this proxy encapsulates a primitive type.
+   *
+   * @return true if this ProxyObject encapsulates a primitive data type
+   */
+  virtual bool isPrimitive();
 
-		/**
-		 * Check if this proxy encapsulates a primitive type.
-		 *
-		 * @return true if this ProxyObject encapsulates a primitive data type
-		 */
-		virtual bool isPrimitive();
+  virtual bool isTemplate();
+  virtual bool isGlobal();
+  virtual bool isConst();
+  virtual bool isStatic();
 
-		virtual bool isTemplate();
-		virtual bool isGlobal();
-		virtual bool isConst();
-		virtual bool isStatic();
-
-	protected:
-		v8::Persistent<v8::Object> proxy; /**< the exposed javascript object */
-
-	};
-
+protected:
+  v8::Persistent<v8::Object> proxy; /**< the exposed javascript object */
+};
 }
 
 #endif /* SRC_OBJECTPROXY_H_ */
