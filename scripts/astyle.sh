16d5f08fc7c7aa4ab106bdc53fae569dc8709f49
Sachin Rajgopal <zyxsachin@hotmail.com>
2016-01-30 16:03:30 +0100

    Edit astyle.sh to indent the public/private keywords

--- scripts/astyle.sh
+++ scripts/astyle.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for filename in $(dirname $0)/../src/*.{cc,h}; do
-	astyle --indent=tab --indent-namespaces -v --suffix=none $filename
+	astyle --indent=tab --indent-classes --indent-namespaces -v --suffix=none $filename
 done
