c0e3d23159288131c9e573f340d917d8c7dd3eaf
chhas <utdtq@student.kit.edu>
2016-02-02 20:12:25 +0100

    add skeleton for class and namespace exposure

--- src/NodeHandler.cc
+++ src/NodeHandler.cc
@@ -1,49 +1,65 @@
 #include "NodeHandler.h"
+
+#include "NodeApplication.h"
 #include "ObjectProxy.h"
 #include "ObjectProxyFactory.h"
 #include "CallbackHandler.h"
-#include "NodeApplication.h"
 
-#include <TROOT.h>
 #include <string>
 
-namespace rootJS {
+#include <TROOT.h>
+#include <TClass.h>
+#include <Rtypes.h>
+#include <TClassTable.h>
+#include <TInterpreter.h>
+
+namespace rootJS
+{
 
 	NodeHandler *NodeHandler::instance;
 	bool NodeHandler::initialized;
 
-	void NodeHandler::initialize(v8::Local<v8::Object> exports, v8::Local<v8::Object> module) {
-		if(!initialized) {
+	void NodeHandler::initialize(v8::Local<v8::Object> exports, v8::Local<v8::Object> module)
+	{
+		if(!initialized)
+		{
 			NodeApplication::CreateNodeApplication();
 			ObjectProxyFactory::initializeProxyMap();
 			instance = new NodeHandler(exports);
 			instance->exposeROOT();
-		} else {
+		}
+		else
+		{
 			v8::Isolate::GetCurrent()->ThrowException(v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), "The NodeHandler can only be initialized once."));
 		}
 	}
 
-	NodeHandler::NodeHandler(v8::Local<v8::Object> exports) {
+	NodeHandler::NodeHandler(v8::Local<v8::Object> exports)
+	{
 		this->exports = exports;
 	}
 
-	void NodeHandler::exposeROOT() {
+	void NodeHandler::exposeROOT()
+	{
 		exposeGlobals();
 		exposeGlobalFunctions();
 	}
 
-	void NodeHandler::exposeGlobals() {
+	void NodeHandler::exposeGlobals()
+	{
 		TCollection *globals = gROOT->GetListOfGlobals(kTRUE);
 
 		TIter next(globals);
-		while(TObject *global = next()) {
+		while(TObject *global = next())
+		{
 			/*
 			 * As we iterate through TObjects all these items can be pumped through
 			 * the ObjectProxyFactory
 			 * TODO: Implement something for scalar globals (often constants)
 			 */
 			ObjectProxy *proxy = ObjectProxyFactory::createObjectProxy(*((TGlobal*)global));
-			if(proxy != nullptr) {
+			if(proxy != nullptr)
+			{
 				v8::Local<v8::String> name = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), global->GetName());
 
 				CallbackHandler::registerGlobalObject(std::string(global->GetName()), proxy);
@@ -58,12 +74,55 @@ namespace rootJS {
 		}
 	}
 
-	void NodeHandler::exposeGlobalFunctions() {
+	void NodeHandler::exposeGlobalFunctions()
+	{
 		TCollection *globals = gROOT->GetListOfGlobalFunctions(kTRUE);
 
 		TIter next(globals);
-		while(TObject *global = next()) {
+		while(TObject *global = next())
+		{
 			NODE_SET_METHOD(this->exports, global->GetName(), CallbackHandler::staticFunctionCallback);
 		}
 	}
+
+	void NodeHandler::exposeClasses()
+	{
+		// gClassTable->PrintTable(); // TODO: remove this line
+
+		gInterpreter->SetClassAutoloading(kTRUE); // maybe not necessary
+
+		for (int i = 0; i < gClassTable->Classes(); i++)
+		{
+
+			DictFuncPtr_t funcPtr = gClassTable->GetDict(gClassTable->At(i));
+			if (funcPtr == nullptr)
+			{
+				continue;
+			}
+
+			TClass *clazz = funcPtr(); // call dictionary function on class
+			if (clazz == nullptr || !clazz->IsLoaded())
+			{
+				continue;
+			}
+
+			if (clazz->Property() & kIsNamespace)
+			{
+				// std::cout << "Namespace: " << clazz->GetName() << std::endl;
+
+			}
+			else if (clazz->Property() & kIsClass)
+			{
+				// std::cout << "Class: " << clazz->GetName() << std::endl;
+
+			}
+			/*
+			 else if (clazz->Property() & ....)
+			 {
+
+			 }
+			 */
+		}
+	}
+
 }
