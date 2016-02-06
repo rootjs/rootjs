56eda2f0bc6eb38ef967a3150e0ba5091de3b960
Jonas Schwabe <jonas@jnugh.de>
2016-02-06 15:37:32 +0100

    Implement BooleanProxy

--- src/BooleanProxy.cc
+++ src/BooleanProxy.cc
@@ -20,4 +20,13 @@ namespace rootJS
 	{
 		// TODO
 	}
+
+	ObjectProxy* BooleanProxy::boolConstruct(MetaInfo& type, TClassRef scope){
+		return new BooleanProxy(type, scope);
+	}
+
+	v8::Local<v8::Value> BooleanProxy::get() {
+		bool* b = (bool*)getAddress();
+		return v8::Boolean::New(v8::Isolate::GetCurrent(), *b);
+	}
 }
