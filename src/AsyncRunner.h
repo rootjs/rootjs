b9f71569a5def7c953e3c109b1172847db271109
Christoph Wolff <icydar@hotmail.com>
2016-02-10 20:19:42 +0100

    Add documentation for AsyncRunner

--- src/AsyncRunner.h
+++ src/AsyncRunner.h
@@ -9,6 +9,10 @@
 
 namespace rootJS {
 
+	/**
+	 * The AsyncRunner provides methods to enable asyncronous function execution.
+	 *
+	 */
 	class AsyncRunner {
 			typedef void (*AsyncFunction)(AsyncRunner* runner, void* param);
 		private:
@@ -18,12 +22,44 @@ namespace rootJS {
 			std::vector<ObjectProxy*> result;
 
 		public:
+			/**
+			 * Creates a new AsyncRunner.
+			 *
+			 * @param func The function to be called asynchronously.
+			 * @param *param The parameters for the function.
+			 * @param callback The callback to be executed in the node thread after the 
+			 * 		asynchronous function is finished.
+			 */
 			AsyncRunner(AsyncFunction func, void *param, v8::Persistent<v8::Function, v8::CopyablePersistentTraits<v8::Function>> callback);
+
+			/**
+			 * Runs the function given with the constructor asynchronously. 
+			 * After it has finished, the callback function is executed in the original node thread.
+			 */
 			void run();
+
+			/**
+			 * Allows the asynchronous function to set its result.
+			 *
+			 * @param result The result of the asynchronous function.
+			 */
 			void setResult(std::vector<ObjectProxy*> result) {
 				this->result = result;
 			};
+
+			/**
+			 * This needs to be implemented for libuv.
+			 *
+			 * @param *req A libuv request.
+			 */
 			static void uvRunner(uv_work_t *req);
+
+			/**
+			 * Executes the callback function.
+			 * 
+			 * @param *req The libuv request, used to get context.	
+			 * @param status If the callback is canceled with uv_cancel() this is UV_ECANCELED.
+			 */
 			static void uvCallback(uv_work_t *req, int status);
 	};
 }
