597bd632210824e98a90037f0dcbaf5303cddcf1
chhas <utdtq@student.kit.edu>
2016-02-11 11:35:52 +0100

    comment unused methods and reformat

--- src/FunctionProxy.cc
+++ src/FunctionProxy.cc
@@ -29,14 +29,47 @@ namespace rootJS
 {
 	std::map<TFunction*, CallFunc_t*> FunctionProxy::functions;
 	std::map<std::string, mappedTypes> FunctionProxy::typeMap = {
-		{"char", mappedTypes::CHAR},
-		{"TString", mappedTypes::TSTRING},
-		{"Int_t", mappedTypes::INT},
-		{"int", mappedTypes::INT},
-		{"Double_t", mappedTypes::DOUBLE},
-		{"Bool_t", mappedTypes::BOOL},
-		//{"_t", mappedTypes::CHAR}
-	};
+	            {"char", mappedTypes::CHAR},
+	            {"TString", mappedTypes::TSTRING},
+	            {"Int_t", mappedTypes::INT},
+	            {"int", mappedTypes::INT},
+	            {"Double_t", mappedTypes::DOUBLE},
+	            {"Bool_t", mappedTypes::BOOL},
+	            //{"_t", mappedTypes::CHAR}
+	        };
+
+	std::vector<TFunction*> FunctionProxy::getMethodsFromName(TClassRef scope, std::string name)
+	{
+		std::vector<TFunction*> methods;
+
+		const TList *funcList = scope.GetClass()->GetListOfAllPublicMethods();
+		TIter funcIter(funcList);
+		TFunction *func;
+		while((func = (TFunction*)funcIter()))
+		{
+			if(name.compare(func->GetName()) == 0)
+			{
+				methods.push_back(func);
+			}
+		}
+
+		return methods;
+	}
+
+	FunctionProxy::FunctionProxy(void *address, FunctionInfo &mode, TFunction *function, TClass *scope) : Proxy(mode, scope)
+	{
+		this->address = address;
+		this->function = function;
+	}
+
+	FunctionProxy* FunctionProxy::clone()
+	{
+		FunctionProxy *p = new FunctionProxy(address, *(rootJS::FunctionInfo*)info, function, scope);
+		p->buf = buf;
+		p->facePtr = facePtr;
+		p->selfAddress = selfAddress;
+		return p;
+	}
 
 	CallFunc_t* FunctionProxy::getCallFunc(const TClassRef& classRef, TFunction* method)
 	{
@@ -113,32 +146,159 @@ namespace rootJS
 		}
 	}
 
-	std::vector<TFunction*> FunctionProxy::getMethodsFromName(TClassRef scope, std::string name)
+	void FunctionProxy::prepareCall(const  v8::Local<v8::Array>& args)
 	{
-		std::vector<TFunction*> methods;
-
-		const TList *funcList = scope.GetClass()->GetListOfAllPublicMethods();
-		TIter funcIter(funcList);
-		TFunction *func;
-		while((func = (TFunction*)funcIter()))
+		CallFunc_t* callFunc = (CallFunc_t*)getCallFunc(scope, function);
+		if(!callFunc)
 		{
-			if(name.compare(func->GetName()) == 0)
-			{
-				methods.push_back(func);
-			}
+			//TODO Handle this, should not segfault (maybe throw something...)
+		}
+		this->facePtr = gCling->CallFunc_IFacePtr( callFunc );
+
+		buf = std::vector<void*>( args->Length() );
+		for(int i = 0; i < (int)args->Length(); i++)
+		{
+			void** bufEl = (void**)malloc(sizeof(void*));
+			*bufEl = bufferParam((TMethodArg*)(function->GetListOfMethodArgs()->At(i)), args->Get(i));
+			buf[i] = bufEl;
+		}
+	}
+
+	ObjectProxy* FunctionProxy::call(bool isConstructorCall /* false */)
+	{
+		void *self = nullptr; //TODO?
+		void *result = nullptr; //TODO?
+
+		switch(facePtr.fKind)
+		{
+		case (TInterpreter::CallFuncIFacePtr_t::kGeneric):
+						facePtr.fGeneric((selfAddress == nullptr) ? self : *(void**)selfAddress, buf.size(), buf.data(), &result);
+			break;
+		case (TInterpreter::CallFuncIFacePtr_t::kCtor):
+				facePtr.fCtor(buf.data(), &result, buf.size());
+			break;
+		default:
+			Toolbox::throwException("Jonas was too lazy to implement this...");
 		}
 
-		return methods;
+		for(int i = 0; i < (int)buf.size(); i++)
+{
+			free(*((void**)buf[i]));
+			free((void*)buf[i]);
+		}
+
+		ObjectProxy* proxy;
+		if(isConstructorCall)
+		{
+			void** ptrptr = (void**)malloc(sizeof(void*)); //TODO: This pointer needs to be freed when the JS Local goes out of scope... Figure out how to do this...
+			*ptrptr = result;
+			PointerInfo mode((void*)ptrptr, function->GetReturnTypeName());
+			proxy = ObjectProxyFactory::createObjectProxy(mode, TClassRef());
+		}
+		else
+		{
+			PointerInfo mode((void*)&result, function->GetReturnTypeName());
+			proxy = ObjectProxyFactory::createObjectProxy(mode, TClassRef());
+		}
+
+		if(proxy)
+		{
+			proxy->backup();
+			return proxy;
+		}
+
+		return nullptr;
 	}
 
-	FunctionProxy::FunctionProxy(void *address, FunctionInfo &mode, TFunction *function, TClass *scope) : Proxy(mode, scope)
+	void* FunctionProxy::bufferParam(TMethodArg* arg, v8::Local<v8::Value> originalArg)
 	{
-		this->address = address;
-		this->function = function;
+		std::map<std::string, mappedTypes>::iterator iterator = typeMap.find(std::string(arg->GetTypeName()));
+		if(iterator == typeMap.end())
+		{
+			Toolbox::throwException(std::string("bufferParam does not know how to handle ") + arg->GetTypeName());
+			return nullptr;
+		}
+		switch(iterator->second)
+		{
+		case mappedTypes::CHAR:
+			return argToChar(originalArg);
+		case mappedTypes::INT:
+			return argToInt(originalArg);
+		case mappedTypes::DOUBLE:
+			return argToDouble(originalArg);
+		case mappedTypes::BOOL:
+			return argToBool(originalArg);
+		case mappedTypes::TSTRING:
+			return argToTString(originalArg);
+		}
+
+		//TODO: This will explode - huge fireball
+		return nullptr;
 	}
+
+	char* FunctionProxy::argToChar(v8::Local<v8::Value> originalArg)
+	{
+		v8::String::Utf8Value string(originalArg->ToString());
+		char *str = (char *) malloc(string.length() + 1);
+		strcpy(str, *string);
+		return str;
+	}
+
+	double* FunctionProxy::argToDouble(v8::Local<v8::Value> originalArg)
+	{
+		double *doubleValue = (double*)malloc(sizeof(double));
+		*doubleValue = getDoubleFromArg(originalArg);
+		return doubleValue;
+	}
+
+	int* FunctionProxy::argToInt(v8::Local<v8::Value> originalArg)
+	{
+		int *intValue = (int *)malloc(sizeof(int));
+		*intValue = (int)getDoubleFromArg(originalArg);
+		return intValue;
+	}
+
+	bool* FunctionProxy::argToBool(v8::Local<v8::Value> originalArg)
+	{
+		bool* boolValue = (bool*)malloc(sizeof(bool));
+		if(originalArg->IsBoolean())
+		{
+			*boolValue = v8::Local<v8::Boolean>::Cast(originalArg)->Value();
+		}
+		else
+		{
+			*boolValue = v8::Local<v8::BooleanObject>::Cast(originalArg)->ValueOf();
+		}
+		return boolValue;
+	}
+
+	TString* FunctionProxy::argToTString(v8::Local<v8::Value> originalArg)
+	{
+		v8::String::Utf8Value string(originalArg->ToString());
+		return new TString(*string);
+	}
+
+	double FunctionProxy::getDoubleFromArg(v8::Local<v8::Value> originalArg)
+	{
+		if(originalArg->IsNumber())
+		{
+			return v8::Local<v8::Number>::Cast(originalArg)->Value();
+		}
+		else if(originalArg->IsNumberObject())
+		{
+			return v8::Local<v8::NumberObject>::Cast(originalArg)->ValueOf();
+		}
+		else
+		{
+			return -1;
+		}
+	}
+
+	/*
+	TODO Verify: validateArgs and determineOverload are combined in FunctionProxyFactory::determineFunction and therefore never used?
 
 	std::vector<ObjectProxy*> FunctionProxy::validateArgs(v8::FunctionCallbackInfo<v8::Value> args)
-	{
+{
 		std::vector<ObjectProxy*> validatedArgs;
 
 		TFunction method = *function;
@@ -243,146 +403,10 @@ namespace rootJS
 		}
 
 		return validatedArgs;
-	}
-
-	char* argToChar(v8::Local<v8::Value> originalArg) {
-		v8::String::Utf8Value string(originalArg->ToString());
-		char *str = (char *) malloc(string.length() + 1);
-		strcpy(str, *string);
-		return str;
-	}
-
-	double getDoubleFromArg(v8::Local<v8::Value> originalArg) {
-		if(originalArg->IsNumber()) {
-			return v8::Local<v8::Number>::Cast(originalArg)->Value();
-		} else if(originalArg->IsNumberObject()) {
-			return v8::Local<v8::NumberObject>::Cast(originalArg)->ValueOf();
-		} else {
-			return -1;
-		}
-	}
-
-	double* argToDouble(v8::Local<v8::Value> originalArg) {
-		double *doubleValue = (double*)malloc(sizeof(double));
-		*doubleValue = getDoubleFromArg(originalArg);
-		return doubleValue;
-	}
-
-	int* argToInt(v8::Local<v8::Value> originalArg) {
-		int *intValue = (int *)malloc(sizeof(int));
-		*intValue = (int)getDoubleFromArg(originalArg);
-		return intValue;
-	}
-
-	bool* argToBool(v8::Local<v8::Value> originalArg) {
-		bool* boolValue = (bool*)malloc(sizeof(bool));
-		if(originalArg->IsBoolean()) {
-			*boolValue = v8::Local<v8::Boolean>::Cast(originalArg)->Value();
-		} else {
-			*boolValue = v8::Local<v8::BooleanObject>::Cast(originalArg)->ValueOf();
-		}
-		return boolValue;
-	}
-
-	TString* argToTString(v8::Local<v8::Value> originalArg) {
-		v8::String::Utf8Value string(originalArg->ToString());
-		return new TString(*string);
-	}
-
-	void* FunctionProxy::bufferParam(TMethodArg* arg, v8::Local<v8::Value> originalArg)
-	{
-		std::map<std::string, mappedTypes>::iterator iterator = typeMap.find(std::string(arg->GetTypeName()));
-		if(iterator == typeMap.end())
-		{
-			Toolbox::throwException(std::string("bufferParam does not know how to handle ") + arg->GetTypeName());
-			return nullptr;
-		}
-		switch(iterator->second)
-		{
-		case mappedTypes::CHAR:
-			return argToChar(originalArg);
-		case mappedTypes::INT:
-			return argToInt(originalArg);
-		case mappedTypes::DOUBLE:
-			return argToDouble(originalArg);
-		case mappedTypes::BOOL:
-			return argToBool(originalArg);
-		case mappedTypes::TSTRING:
-			return argToTString(originalArg);
-		}
-
-		//TODO: This will explode - huge fireball
-		return nullptr;
-	}
-
-	void FunctionProxy::prepareCall(const  v8::Local<v8::Array>& args)
-	{
-		CallFunc_t* callFunc = (CallFunc_t*)getCallFunc(scope, function);
-		if(!callFunc)
-		{
-			//TODO Handle this, should not segfault (maybe throw something...)
-		}
-		this->facePtr = gCling->CallFunc_IFacePtr( callFunc );
-
-		buf = std::vector<void*>( args->Length() );
-		for(int i = 0; i < (int)args->Length(); i++) {
-			void** bufEl = (void**)malloc(sizeof(void*));
-			*bufEl = bufferParam((TMethodArg*)(function->GetListOfMethodArgs()->At(i)), args->Get(i));
-			buf[i] = bufEl;
-		}
-	}
-
-	ObjectProxy* FunctionProxy::call(bool isConstructorCall /* false */)
-	{
-		void *self = nullptr; //TODO?
-		void *result = nullptr; //TODO?
-		switch(facePtr.fKind) {
-		case (TInterpreter::CallFuncIFacePtr_t::kGeneric):
-			facePtr.fGeneric((selfAddress == nullptr)?self:*(void**)selfAddress, buf.size(), buf.data(), &result);
-			break;
-		case (TInterpreter::CallFuncIFacePtr_t::kCtor):
-			facePtr.fCtor(buf.data(), &result, buf.size());
-			break;
-		default:
-			v8::Isolate::GetCurrent()->ThrowException(v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), "Jonas was too lazy to implement this..."));
-		}
-
-		for(int i = 0; i < (int)buf.size(); i++)
-		{
-			free(*((void**)buf[i]));
-			free((void*)buf[i]);
-		}
-
-		ObjectProxy* proxy;
-		if(isConstructorCall) {
-			void** ptrptr = (void**)malloc(sizeof(void*)); //TODO: This pointer needs to be freed when the JS Local goes out of scope... Figure out how to do this...
-			*ptrptr = result;
-			PointerInfo mode((void*)ptrptr, function->GetReturnTypeName());
-			proxy = ObjectProxyFactory::createObjectProxy(mode, TClassRef());
-		} else {
-			PointerInfo mode((void*)&result, function->GetReturnTypeName());
-			proxy = ObjectProxyFactory::createObjectProxy(mode, TClassRef());
-		}
-
-		if(proxy) {
-			proxy->backup();
-			return proxy;
-		}
-
-		return nullptr;
-	}
-
-	FunctionProxy* FunctionProxy::clone()
-	{
-		FunctionProxy *p = new FunctionProxy(address, *(rootJS::FunctionInfo*)info, function, scope);
-		p->buf = buf;
-		p->facePtr = facePtr;
-		p->selfAddress = selfAddress;
-		return p;
-	}
+}
 
 	bool FunctionProxy::determineOverload(const v8::Local<v8::Array>& info)
-	{
+{
 		TFunction* overloadedFunction = FunctionProxyFactory::determineFunction(function->GetName(), scope.GetClass(), info);
 		if(overloadedFunction == nullptr)
 		{
@@ -390,29 +414,29 @@ namespace rootJS
 		}
 		function = overloadedFunction;
 		return true;
-	}
-
+}
+	*/
 
 	/*
 	// TODO
 	bool FunctionProxy::processCall(TFunction* method, void* args, void* self, void* result)
-	{
-	}
+{
+}
 
 	void* FunctionProxy::callConstructor(TFunction* method, TClassRef type, void* args)
-	{
-	}
+{
+}
 
 	void FunctionProxy::callDestructor(TClassRef type, void* self)
-	{
-	}
+{
+}
 
 	void* FunctionProxy::callObject(TFunction* method, void* self, void* args, TClassRef resType)
-	{
-	}
+{
+}
 
 	template <typename T>
 	T FunctionProxy::callPrimitive(TFunction* method, void* self, void* args)
-	{
-	}*/
+{
+}*/
 }
