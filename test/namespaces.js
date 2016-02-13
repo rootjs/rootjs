9beddb826fc556bebbf3f93eaf79fd53e6b118e2
Theo Beffart <tbeffart@gmail.com>
2016-02-13 01:04:33 +0100

    update tests

--- test/namespaces.js
+++ test/namespaces.js
@@ -9,5 +9,19 @@ describe('Namespaces', function() {
 			  should.not.equal(root.ROOT.Detail.TSchemaRuleSet, undefined);
 		  }).should.not.throw();
 	  });
+	  it('should have the property ROOT.Detail', function() {
+		  (function() {
+			  should.not.equal(root.Detail, undefined);
+		  }).should.not.throw();
+	  })('should have the property ROOT.TStreamerInfoActions', function() {
+		  (function() {
+			  should.not.equal(root.TStreamerInfoActions, undefined);
+		  }).should.not.throw();
+	  });
+	  it('should have the property TString', function() {
+		  (function() {
+			  should.not.equal(root.TString, undefined);
+		  }).should.not.throw();
+	  });
   });
 });
