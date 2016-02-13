024caa54f62114c2a2d3ab83e842c85b94b269fd
Sachin Rajgopal <zyxsachin@hotmail.com>
2016-02-13 15:38:43 +0100

    Add comments and modify Doxyfile

--- src/Types.h
+++ src/Types.h
@@ -8,10 +8,33 @@ namespace rootJS
 	class Types
 	{
 		public:
+            /**
+             * Checks if the given value is an v8 Boolean.
+             *
+             * @param value
+             *              The value to checked
+             */
 			static bool isV8Boolean(v8::Local<v8::Value> value);
+            /**
+             * Checks if the given value is an v8 Number.
+             *
+             * @param value
+             *              The value to checked
+             */
 			static bool isV8Number(v8::Local<v8::Value> value);
+            /**
+             * Checks if the given value is an v8 String.
+             *
+             * @param value
+             *              The value to checked
+             */
 			static bool isV8String(v8::Local<v8::Value> value);
-
+            /**
+             * Checks if the given value is an v8 Primitive.
+             *
+             * @param value
+             *              The value to checked
+             */
 			static bool isV8Primitive(v8::Local<v8::Value> value);
 	};
 }
