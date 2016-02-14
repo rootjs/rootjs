c279d0fc259349507482333ee7c71add4a55eecd
Theo Beffart <tbeffart@gmail.com>
2016-02-14 03:09:26 +0100

    add test for lib loading

--- test/loading.js
+++ test/loading.js
@@ -5,15 +5,14 @@ var root = require('../index');
 
 describe('Dynamic Loading', function() {
     describe('interface', function () {
-        it('should be possible to call loadlibrary to load libASImageGui', function () {
+        it('should be possible to call loadlibrary to load MathCore', function () {
             (function () {
-                root.loadlibrary("libASImageGui.so").should.equal(0);
+              root.loadlibrary("libMathCore.so").should.equal(0);
             }).should.not.throw();
         });
-        it('should be possible to trigger a refresh of the exported functions', function (){
-            (function () {
-                root.refreshExports();
-            }).should.not.throw();
+        it('should be possible to call stuffi', function () {
+               root.ROOT.Math.should.not.be.undefined()
+
         });
     });
 });
