6d0421d90574004fbd262c85c20b164ee5f913e1
Theo Beffart <tbeffart@gmail.com>
2016-02-14 04:17:40 +0100

    update test

--- test/loading.js
+++ test/loading.js
@@ -4,15 +4,22 @@ var root = require('../index');
 
 
 describe('Dynamic Loading', function() {
+    this.timeout(10000);
     describe('interface', function () {
         it('should be possible to call loadlibrary to load MathCore', function () {
             (function () {
-              root.loadlibrary("libMathCore.so").should.equal(0);
+                root.loadlibrary("libMathCore.so");
             }).should.not.throw();
         });
-        it('ROOT.Math should not be undefined anymore', function () {
-               root.ROOT.Math.should.not.be.undefined()
 
+        describe('accessing', function () {
+            it('ROOT.Math should not be undefined anymore', function () {
+                root.ROOT.Math.should.not.be.undefined()
+            });
+
+            it('pi should be equal to 3.141592653589793', function () {
+                root.ROOT.Math.Pi().should.be.equal(3.141592653589793);
+            });
         });
     });
 });
