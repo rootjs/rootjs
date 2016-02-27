3206686140143fe7440baa602ac4f2652e2776b0
Jonas Schwabe <jonas@jnugh.de>
2016-02-27 20:54:28 +0100

    Coverage reporting

--- nodejs.sh
+++ nodejs.sh
@@ -3,5 +3,8 @@ cd $WORKSPACE/rootinstall
 source ./bin/thisroot.sh
 cd $WORKSPACE
 rm artifacts/* -R
-npm install
+ROOTJS_ADD_CFLAGS="-fprofile-arcs -ftest-coverage" ROOTJS_ADD_LDLAGS="-lgcov --coverage" npm install
 npm test
+
+lcov --capture --base-directory src/ --directory build/Release/obj.target/rootjs/src/  --output-file coverage.info --no-external
+genhtml coverage.info --output-directory coverage
